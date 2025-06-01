b41abe4a464941ceb71382fce8df9508cfb0bea6
Tommy <35646257+tom1919@users.noreply.github.com>
2025-05-31 21:01:36 -0400

    Add files via upload

---
+++ ipy_startup.py
@@ -0,0 +1,10 @@
+# This file will be executed every time a new IPython session is started.
+# goes in startup folder of ipython profile (terminal: ipython profile locate)
+
+
+try:
+    get_ipython().run_line_magic('load_ext', 'autoreload')
+    get_ipython().run_line_magic('autoreload', '2')
+except NameError:
+    # This will happen if get_ipython() is not defined (e.g., in a plain Python script)
+    pass # Do nothing, autoreload is not for non-IPython environments
